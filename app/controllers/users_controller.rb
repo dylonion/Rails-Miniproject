b4eed56af7cfcd36da1811690da74a6c8e55abc2
Dylan Moylan <dylanmoylan1@gmail.com>
2017-12-13 17:11:42 -0500

    Initial tests with AWS. May be removed later.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApiController
   before_action :require_login, except: [:create]
+  before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
   def create
     puts "test #{user_params}"
@@ -20,5 +21,7 @@ class UsersController < ApiController
   def user_params
     params.require(:user).permit(:username, :email, :password, :name)
   end
-
+  def set_s3_direct_post
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+  end
 end
